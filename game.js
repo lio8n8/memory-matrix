212de7e86036737c39b6015c20c5c0034098df9b
lio8n8 <lio8n8@gmail.com>
2019-03-23 13:52:38 +0200

    Init game

--- game.js
+++ game.js
@@ -1,6 +1,19 @@
+/**
+ * Memory Matrix
+ * 
+ * Try to repeat random generated pattern.
+ * Game Board consists of N tiles (rows * columns )
+ * For every success of repeating pattern GameBoard increases by one row or column
+ * (till to max possible number of rows r columns). For every failure number of rows 
+ * or colums decrease by one (till to min possible values of rows and colums).
+ */
 class Game {
-    constructor() {
+    constructor(GameBoard) {
+        this.gameBoard = new GameBoard(4, 4, RNG);
+    }
 
+    start() {
+        this.gameBoard.init();
     }
 
     draw() {
@@ -16,8 +29,58 @@ class User {
 
 }
 
+/**
+ * Renders tiles.
+ */
 class GameBoard {
+    static MIN_ROWS = 4;
+    static MIN_COLS = 4;
+    static MAX_ROWS = 15;
+    static MAX_COLS = 15;
 
+    constructor(rows = 4, columns = 4, RNG) {
+        this.rows = rows;
+        this.colums = columns;
+        this.randomTiles = 4;
+        this.tiles = [];
+        this.gameBoardEl = document.getElementById('game-board');
+        this.rng = new RNG();
+    }
+
+    init() {
+        this.rng.generate(this.randomTiles);
+
+        for (let i = 0; i < this.rows; i++) {
+            this.gameBoardEl.appendChild(this.createRow());
+        }
+    }
+
+    createRow() {
+        let row = document.createElement('div');
+        let tile = null;
+        row.className = 'tile-row';
+
+        for (let i = 0; i < this.colums; i++) {
+            tile = document.createElement('div');
+            tile.className = 'tile';
+            this.tiles.push(tile);
+            row.appendChild(tile);
+        }
+
+        return row;
+    }
+
+    createColumn() {
+
+    }
+
+    removeRow() {
+
+    }
+
+    removeColumn() {
+
+    }
 }
 
 class LevelManager {
@@ -28,5 +91,36 @@ class LevelManager {
  * Random number generator.
  */
 class RNG {
+    constructor() {
+        this.numbers = [];
+    }
 
+    /**
+     * Creates array of unique random integers.
+     * @param {Number} size Length of array.
+     * @param {Number} max Maximum value of number.
+     * 
+     * @returns {Array[Number]} Array of unique random numbers.
+     */
+    generate(size, max = 16) {
+        this.numbers = [];
+        let number;
+
+        for (let i = 0; i < size; i++) {
+            do {
+                number = Math.round(Math.random() * (max - 1));
+            } while (this.numbers.includes(number))
+
+            this.numbers.push(number);
+        }
+
+        return this.numbers;
+    }
+
+    getNumbers() {
+        return this.numbers;
+    }
 }
+
+const game = new Game(GameBoard);
+game.start();
